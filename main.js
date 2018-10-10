b5b8392fa97c269b0901195d8e218b1e1cedd559
obayomi96 <martinsoluwaseun47@gmail.com>
2018-10-10 13:42:12 +0000

    dropdwon display modified

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // dropdown menu function
-toggler.onclick = () => {
-    const toggler = document.getElementBtId("toggler");
+const toggler = document.getElementBtId("toggler");
+    toggler.onclick = () => {
+    modal.style.display = "block";
 } 
+
  // close menu onlick outside the dowpdown
  window.addEventListener('mouseup', (event) => {
      const modal = document.getElementById("navs");
