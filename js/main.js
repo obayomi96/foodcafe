80e716b31619deb160e3916f7ba31992ff1e59ed
obayomi96 <martinsoluwaseun47@gmail.com>
2018-12-11 12:38:21 +0000

    Refactored dropdown toggler

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // dropdown menu function
+const dropMenu = document.getElementById("navs");
 const toggler = document.getElementById("toggler");
+
     toggler.onclick = () => {
     dropMenu.style.display = "block"; 
 
  // close menu onlick outside the dowpdown
- window.addEventListener('mouseup', (event) => {
-     const dropMenu = document.getElementById("navs");
-     
+ window.addEventListener('mouseup', (event) => {    
     if(event.target != dropMenu && event.target.parentNode != dropMenu) {
       dropMenu.style.display = "none";
     }
